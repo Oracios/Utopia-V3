f99b3af2ce7d1c5e75eac88ed87a971a55a1d9f4
Oracios <rether.matthieu@outlook.fr>
2025-06-21 00:45:17 +0200

    V1.0.3
    V1.0.3

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -32,9 +32,9 @@ class Splash {
         let splashes = [
             { "message": "HOU, HOU, HOU !!!", "author": "Oracios" },
             { "message": "Sport national Utopien, le jeu de la biscotte", "author": "Sapfhusaa" },
-            { "message": "Utopièces je m'en fou pleins les fesses !", "author": "Flmby" }
-            { "message": "Ca ma prit 3 semaines pour faire ça...", "author": "LightShadow02" }
-            { "message": "J'ai vendu mes cheveux..", "author": "MrBurritos" }
+            { "message": "Utopièces je m'en fou pleins les fesses !", "author": "Flmby" },
+            { "message": "Ca ma prit 3 semaines pour faire ça...", "author": "LightShadow02" },
+            { "message": "J'ai vendu mes cheveux..", "author": "MrBurritos" },
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
